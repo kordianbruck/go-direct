e593c3a81dc62ff24fc0ef92cd5450a66c7a61a6
Kordian Bruck <k@bruck.me>
2018-10-08 11:36:30 +0200

    Add parsing of env variables

--- src/main.go
+++ src/main.go
@@ -1,16 +1,28 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
+	"os"
 )
 
 func main() {
+	url, ok := os.LookupEnv("URL")
+	if !ok {
+		url = "https://google.com"
+	}
+	port, ok := os.LookupEnv("PORT")
+	if !ok {
+		port = "80"
+	}
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Location", "https://google.com")
+		w.Header().Set("Location", url)
 		w.WriteHeader(301)
 	})
 
-	if err := http.ListenAndServe(":80", nil); err != nil {
+	fmt.Printf("Starting redirection server on port %v with redirection to %v", port, url)
+	if err := http.ListenAndServe(":"+port, nil); err != nil {
 		panic(err)
 	}
 }
